eb1b47938cf0f6480bf9d7230298caf403b92b72
mokaddes1995 <monarulislam443@gmail.com>
2026-09-15 20:03:30 +0600

    Class Object Crated and Call Object Method

--- Class/assignment_3.py
+++ Class/assignment_3.py
@@ -11,3 +11,24 @@ class Laptop:
         self.brand = brand
         self.processor = processor
         self.price = price
+        
+    # Instance Method for Print Information
+    
+    def display_info(self):
+        print()
+        print("Print All Info")
+        print('*' * 30)
+        print(f"Brand: {self.brand}")
+        print(f"Processor: {self.processor}")
+        print(f"Price: {self.price}")
+        
+    
+#Create an Object Name Laptop1
+
+laptop1 = Laptop("Asus", "Inter Core i-7", 190000)
+
+# Call Object Method for Display Info
+
+laptop1.display_info()
+        
+        
