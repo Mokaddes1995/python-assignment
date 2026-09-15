61fe9a15217160403cad04ee099a417130b109c9
mokaddes1995 <monarulislam443@gmail.com>
2026-09-16 00:56:45 +0600

    Assignmen_3 Not Complete

--- Class/assignment_3.py
+++ Class/assignment_3.py
@@ -181,46 +181,266 @@
 # temperature1.check_health()
 
 
-# Task 5
+# # Task 5
+
+# '''
+# Task 5 (Inheritance): Create a parent class Vehicle with a method move().
+# Create a child class Airplane that inherits from Vehicle and adds a unique method fly().
+# '''
+
+# # Create Parent Class Vehicle
+
+# class Vehicle:
+    
+#     # Instance Method
+    
+#     def move(self):
+        
+#         print("The vehicle is moving.")
+
+
+
+# # Create Child Class Airplane
+
+# class Airplane(Vehicle):    # Inheritance Vehicle Class
+    
+#     # Instance Method
+    
+#     # Unique Method fly
+    
+#     def fly(self):
+        
+#         print("The Airplane Flying")
+        
+
+# # Create an Object
+
+# airplane1 = Airplane()
+
+# # Call Inheritance Method
+
+# airplane1.move()
+
+# # Call Unique Method
+
+# airplane1.fly()
+
+
+# Section 2: Scenario-based Challenges
 
 '''
-Task 5 (Inheritance): Create a parent class Vehicle with a method move().
-Create a child class Airplane that inherits from Vehicle and adds a unique method fly().
+
+ATM System: Create a class named Class. The account should contain: 
+Account number, Account holder name, Balance. Implement the following 
+methods: deposit(), withdraw(), show_balance(). The system must: 
+○  Allow users to deposit and withdraw money. 
+○  Prevent withdrawal when the balance is insufficient. 
+○  Display the current balance. 
+○  Create at least two different accounts. 
+○  Perform transactions on both accounts. 
+
 '''
 
-# Create Parent Class Vehicle
-
-class Vehicle:
+class BankAccount:
     
+    # Constructor Method
+    
+    def __init__(self, account_number, name, balance):
+        self.account_number = account_number
+        self.account_holder_name = name
+        self.__balance = balance            # Encapsulation Method for Private Balance
+        
+    # Instance Method 
+    
+    # Method Show Balance
+    
+    def show_balance(self):
+        print(f"Balance: {self.__balance:.2f}")
+        
+    # Method Deposit Balance
+    
+    def deposit(self, amount):
+        
+        # Validation for O and Negative amounts
+        
+        if amount <= 0:
+            raise ValueError("Amount Must be 1 and above!!")
+        
+        
+        self.__balance += amount
+        
+        print(f"${amount:.2f} Deposit Successful..")
+        
+        self.show_balance()
+        
+        
+    # Method Withdraw Balance
+    
+    def withdraw(self, amount):
+        
+        # Validation for 0 and Negative amounts
+        
+        if amount <= 0:
+            raise ValueError ("Amount Must be 1 and above!!")
+        
+        # Prevent withdrawal when the balance is insufficient.
+        
+        if amount > self.__balance:
+            raise ValueError ('Insufficient Balance!!!')
+        
+        self.__balance -= amount
+        
+        print(f"${amount:.2f} Withdraw Successful..")
+        
+        self.show_balance()
+    
+
+
+# Create two different accounts.
+
+ba1 = BankAccount(13456, "Md Mokaddes Hossain", 5000)
+
+ba2 = BankAccount(13457, "Maimun Al Rafat", 10000)
+
+# Display the current balance
+
+ba1.show_balance()
+ba2.show_balance()
+
+# Deposit Money with Error Handling
+
+try:
+    ba1.deposit(5000)
+    
+except ValueError as e:
+    
+    print(e)
+    
+
+try:
+    ba2.deposit(3000)
+    
+except ValueError as e:
+    
+    print(e)
+    
+    
+# Withdraw Money with Error Handling
+
+try:
+    ba1.withdraw(7000)
+    
+except ValueError as e:
+    
+    print(e)
+    
+
+try:
+    ba2.withdraw(12000)
+    
+except ValueError as e:
+    
+    print(e)
+    
+    
+
+
+'''
+Library Management System: Create two classes named Book and Library. 
+In Book class, each book should contain: Title, Author, ISBN, Availability Status. 
+In Library class, the library should provide methods to: 
+add_method(), borrow_book(), return_book(), display_books(). The 
+
+system should: 
+○  Allow books to be added to the library. 
+○  Display all available books. 
+○  Allow user to borrow a book 
+○  Prevent a book from being borrowed if it is already borrowed. 
+○  Allow borrowed books to be returned. 
+○  Update the availability status automatically. 
+'''
+
+# Create Book Class
+
+class Book:
+    
+    # Constructor Method
+    
+    def __init__(self, title, author, isbn, is_availability):
+        
+        self.title = title
+        self.author = author
+        self.isbn = isbn
+        
+        self.is_available = True  
+        
+
+
+# Create Library Class
+
+class Library():
+    
+    # Constructor Method
+    
+    def __init__(self):
+        
+        self.book = []
+    
+            
     # Instance Method
     
-    def move(self):
-        
-        print("The vehicle is moving.")
-
-
-
-# Create Child Class Airplane
-
-class Airplane(Vehicle):    # Inheritance Vehicle Class
+    # Method Add
     
-    # Instance Method
-    
-    # Unique Method fly
-    
-    def fly(self):
+    def add_method(self, books):
         
-        print("The Airplane Flying")
+        self.book.append(self.books)
+    
+    # Method borrow book
+    
+    def borrow_book(self):
         
+        if self.is_availability:
+            
+            self.is_availability = False
+            
+            print("Borrow the Book")
+            
+        else:
+            
+            print("Already borrowed")
+    
+    # Method return book
+    
+    def return_book(self):
+        
+        if not self.is_availability:
+            
+            self.is_availability = True
+            
+            print("Book Return")
+            
+        else:
+            
+            print("The Book Already Returned!!!")
+    
+    
+    # Method display book
+    
+    def display_books(self):
+        
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+        print(f"ISBN: {self.isbn}")
+        print(f"Available:{self.is_availability}")
+        
+        
+# Crate Library Object
 
-# Create an Object
+book1 = Library('Python Programming', 'Jon Luise', 'A12756B', True)
 
-airplane1 = Airplane()
+book1.display_books()
 
-# Call Inheritance Method
+book1.add_method('Python', 'John Doe', 'A12756B', True)
 
-airplane1.move()
-
-# Call Unique Method
-
-airplane1.fly()
+book1.display_books()
+        
